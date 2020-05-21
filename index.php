c9e28aa435fecb7e07a895d42cb4afb3b4b07d70
JudithKurian <judithkurian@karunya.edu.in>
2020-05-21 15:56:02 +0530

    Fixed the star positioning issue

--- index.php
+++ index.php
@@ -99,6 +99,12 @@
    display:none;
   }
 
+  #ratinglabel
+  {
+    position:relative;
+    top:1.4rem;
+  }
+
     </style>
   </head>
   <body>
@@ -134,7 +140,7 @@
               </div>
             </div>
           <div class="form-group row">
-              <label for="star_rating" class="col-sm-6">Work Rating<span class="star" style="color:red">*</span></label>
+              <label for="star_rating" class="col-sm-6" id="ratinglabel">Work Rating<span class="star" style="color:red">*</span></label>
               <div class="col-sm-6">
             <div class="rating"> <input type="radio" name="rating" value="5" id="5"><label for="5">☆</label> <input type="radio" name="rating" value="4" id="4"><label for="4">☆</label> <input type="radio" name="rating" value="3" id="3"><label for="3">☆</label> <input type="radio" name="rating" value="2" id="2"><label for="2">☆</label> <input type="radio" name="rating" value="1" id="1"><label for="1">☆</label>
 </div>
